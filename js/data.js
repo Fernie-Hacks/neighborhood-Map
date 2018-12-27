760d2bb4746c0b45c81151059314340159a82d05
Fernando Gonzalez <fgonz009@ucr.edu>
2018-12-27 14:46:10 -0800

    Initial code with map and nav.

---
+++ js/data.js
@@ -0,0 +1,49 @@
+/*
+ *  Model Data
+ *  	Locations in the Riverside County
+ */
+
+var locations = [
+    {
+        title: 'Perris High School',
+        location: {
+            lat: 33.8007538,
+            lng: -117.22364399999998
+        }
+    },
+    {
+        title: 'University of California, Riverside',
+        location: {
+            lat: 33.9756518,
+            lng: -117.33111059999999
+        }
+    },
+    {
+        title: 'In & Out',
+        location: {
+            lat: 33.9421134,
+            lng: -117.26013929999999
+        }
+    },
+    {
+        title: 'The "C"',
+        location: {
+            lat: 33.9756409,
+            lng: -117.312588
+        }
+    },
+    {
+        title: 'AhiPoki Bowl',
+        location: {
+            lat: 33.9393618,
+            lng: -117.2785237
+        }
+    },
+    {
+        title: 'Chicago Pasta House',
+        location: {
+            lat: 33.9387766,
+            lng: -117.23218710000003
+        }
+    }
+];
